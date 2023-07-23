5d6af89467e1063be804be5c488e6df581f57c3a
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-23 17:37:52 +0000

    fetching qoute from backend

---
+++ src/components/CarouselWithQuote/CarouselWithQoute.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const CarouselWithQuote = () => {
+    const [quote, setQuote] = useState('');
+
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+
+    const fetchQuote = async () => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_QOUTE_URL}`); // Replace with the actual API URL
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            const data = await response.json();
+            console.log(data);
+            setQuote(data.content);
+        } catch (error) {
+            console.log('Error fetching quote:', error);
+        }
+    };
+
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <div className="carousel-content">
+                    <p>{quote}</p>
+                </div>
+            </Carousel.Item>
+            {/* Add more Carousel items if needed */}
+        </Carousel>
+    );
+};
+
+export default CarouselWithQuote;
