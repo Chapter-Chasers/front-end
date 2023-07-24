e716943bc2423572803e8ea2c6be5aa4e195451c
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-24 18:08:46 +0000

    styling the form

--- src/components/emailsub/Emailsub.js
+++ src/components/emailsub/Emailsub.js
@@ -1,17 +1,28 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import './email.css'
+import { Container } from 'react-bootstrap';
 
-
+function handleSubmit(){
+  
+}
 
 function Emailsub() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-        <Button variant="primary">submit</Button>
-      </Form.Group>
-    </Form>
+    <section className='email_sub d-flex justify-content-center align-items-center'>
+      <Container className='d-flex flex-column'>
+        <h2>Subscribe to our Daily Qoute</h2>
+        <Form onSubmit={()=>handleSubmit} className='w-50'>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="John Raze" />
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+            <Button type='submit' variant="dark">submit</Button>
+          </Form.Group>
+        </Form>
+      </Container>
+    </section>
   );
 }
 export default Emailsub;
