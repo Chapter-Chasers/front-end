df8f987017e9b73389d117eaedfd6166a86f4ac4
hayahas <haya-nael@hotmail.com>
2023-07-26 08:30:29 +0000

    payment page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import Home from './components/Home/Home';
 import Navbar from './components/NavBar/NavBar';
 import Cart from './components/cart/Cart';
+import Cart2 from './components/cart/Cart2';
 import Footer from './components/footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Qoutes from "./components/qoutes/Qoutes";
@@ -24,6 +25,7 @@ function App() {
         <Route path="/finishedBooks" element={<Home />} />
         <Route path="/qoutes" element={<Qoutes />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Cart2 />} />
         <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/bookDetails/:id" element={<BookDetails />} />
       </Routes>
